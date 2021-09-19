3252cad8706e45fc03bb6365e64d99782b1386eb
raden-dimas012 <dimascode012@gmail.com>
2021-09-19 18:10:01 +0700

    Initial Commit

---
+++ DicodingSwiftPemula/DicodingSwiftPemulaApp.swift
@@ -0,0 +1,17 @@
+//
+//  DicodingSwiftPemulaApp.swift
+//  DicodingSwiftPemula
+//
+//  Created by Raden Dimas on 19/09/21.
+//
+
+import SwiftUI
+
+@main
+struct DicodingSwiftPemulaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
